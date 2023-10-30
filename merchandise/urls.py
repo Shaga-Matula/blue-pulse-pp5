f39d4719bbac0d65ec1b5ee6e281c0ac2ce2e711
Paul-Gleeson <Shagamatula@gmail.com>
2023-10-30 18:00:55 +0000

    feat(merchandise): add all_merch.html template

--- merchandise/urls.py
+++ merchandise/urls.py
@@ -1,7 +1,8 @@
 
 from django.contrib import admin
 from django.urls import path
+from .views import AllMerchandiseView
 
 urlpatterns = [
-
+    path('all_merchandise/', AllMerchandiseView.as_view(), name='all_merchandise'),
 ]
--- merchandise/views.py
+++ merchandise/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import TemplateView
+from .models import MerchandiseMod
+from django.views.generic.detail import DetailView
 
-# Create your views here.
+
+class AllMerchandiseView(TemplateView):
+    """
+    Gets All Merchandise
+    """
+    model = MerchandiseMod
+    template_name = 'merchandise/all_merch.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['all_merch'] = MerchandiseMod.objects.all()
+        return context
