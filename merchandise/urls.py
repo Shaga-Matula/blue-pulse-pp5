21fb533456583bef95c0e8e3856cd7880129e945
Paul-Gleeson <Shagamatula@gmail.com>
2023-11-02 11:02:08 +0000

    feat(merchandise): Implemented search functionality in views.py and adjusted urls.py accordingly

--- merchandise/urls.py
+++ merchandise/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import AllMerchView, MerchendiseDetailView
+from .views import AllMerchView, MerchandiseDetailView
 
 
 urlpatterns = [
     path('all_merchandise/', AllMerchView.as_view(), name='all_merchandise'),
-    path('all_merchandise/<int:pk>/', MerchendiseDetailView.as_view(), name='merch_item')
+    path('all_merchandise/<int:pk>/', MerchandiseDetailView.as_view(), name='merch_item')
 ]
--- merchandise/views.py
+++ merchandise/views.py
@@ -2,22 +2,39 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic import TemplateView
 from .models import MerchandiseMod
 from django.views.generic.detail import DetailView
+from django.contrib import messages
+from django.db.models import Q
 
 
 class AllMerchView(TemplateView):
     """
-    Gets All Merchandise
+    Render: all merchandise and handles search Q
     """
-    model = MerchandiseMod
     template_name = 'merchandise/all_merch.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['all_merch'] = MerchandiseMod.objects.all()
+        search_query = self.request.GET.get('q')
+        # Search Query using Q query 
+        if search_query:
+            queries = Q(name__icontains=search_query) | Q(description__icontains=search_query)
+            all_merch = MerchandiseMod.objects.filter(queries)
+            if all_merch:
+                context['all_merch'] = all_merch
+                messages.success(self.request, f"Search items results successfully retrieved for '{search_query}'")
+            else:
+                context['all_merch'] = []
+                messages.warning(self.request, f"Sorry no items retrieved for '{search_query}'")
+        else:
+            context['all_merch'] = MerchandiseMod.objects.all()
+
         return context
 
 
-class MerchendiseDetailView(DetailView):
+class MerchandiseDetailView(DetailView):
+    """
+    Render: an enlarged single item on click 
+    """
     model = MerchandiseMod
     template_name = 'merchandise/merch_item.html'
     context_object_name = 'merch_item'
