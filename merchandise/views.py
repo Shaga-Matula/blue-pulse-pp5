a2b94aacba60fb1c8120e71b5846f49c9dbbc671
Paul-Gleeson <Shagamatula@gmail.com>
2023-11-04 12:23:18 +0000

    feat(ui): added second navigation bar and improved CSS

--- merchandise/views.py
+++ merchandise/views.py
@@ -7,26 +7,30 @@ from django.db.models import Q
 
 
 class AllMerchView(TemplateView):
-    """
-    Render: all merchandise and handles search Q
-    """
     template_name = 'merchandise/all_merch.html'
-
+    
+    query = None
+    categories = None
+    
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         search_query = self.request.GET.get('q')
-        # Search Query using Q query 
+        category_name = self.request.GET.get('category')  # Get the 'category' query parameter
+
+        all_merch = MerchandiseMod.objects.all()
+
+        if category_name:
+            # ForeignKey
+            all_merch = all_merch.filter(category__name=category_name)
+
         if search_query:
             queries = Q(name__icontains=search_query) | Q(description__icontains=search_query)
-            all_merch = MerchandiseMod.objects.filter(queries)
-            if all_merch:
-                context['all_merch'] = all_merch
-                messages.success(self.request, f"Search items results successfully retrieved for '{search_query}'")
-            else:
-                context['all_merch'] = []
-                messages.warning(self.request, f"Sorry no items retrieved for '{search_query}'")
-        else:
-            context['all_merch'] = MerchandiseMod.objects.all()
+            all_merch = all_merch.filter(queries)
+
+        context['all_merch'] = all_merch
+
+        if not all_merch:
+            messages.warning(self.request, f"Sorry, no items retrieved for '{search_query}'")
 
         return context
 
