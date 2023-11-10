91bbb99fb4d2cbeb186ae12ceebbfdf8b6ee0aac
Paul-Gleeson <Shagamatula@gmail.com>
2023-11-10 14:27:07 +0000

    feat(Stripe): Implemented CSS and JS for Stripe integration in checkout app

--- checkout/views.py
+++ checkout/views.py
@@ -14,7 +14,10 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51NvFG2KnbYEnx5OfpEnDXH3ibrWRTB8pTuGf0njsHe3s3FCHAkz4Xh2NG2N7v84Fb6tSYDg5rHPVsUBs9rUZNnQa00XecMkKdS',
+        'client_secret': 'test client secret',
     }
+
 
     return render(request, template, context)
 
