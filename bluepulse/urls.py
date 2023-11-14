4285d967769eacc1b0f049dd7fcc01fc79fbd3a5
Paul-Gleeson <Shagamatula@gmail.com>
2023-11-14 12:32:19 +0000

    feat(profiles): Create user profile database and urls

--- bluepulse/urls.py
+++ bluepulse/urls.py
@@ -1,17 +1,17 @@
 """ Blue Pulse URL Configuration """
 
 
-from django.contrib import admin
-from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),
-    path('', include('home.urls')),
-    path('', include('merchandise.urls')),
-    path('checkout/', include('checkout.urls')),
-    path('', include('bag.urls'))
-    path('', include('profiles.urls'))
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path("admin/", admin.site.urls),
+    path("accounts/", include("allauth.urls")),
+    path("", include("home.urls")),
+    path("", include("merchandise.urls")),
+    path("checkout/", include("checkout.urls")),
+    path("", include("bag.urls")),
+    path("profile/", include("profiles.urls")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- profiles/urls.py
+++ profiles/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
 
+from . import views
 
-
-urlpatterns = [
-
-]
+urlpatterns = [path("", views.profile, name="profile")]
