f2065865e4f919a666e1624896fc28b0aca589b5
Paul-Gleeson <Shagamatula@gmail.com>
2023-11-25 15:54:03 +0000

    feat(contacts): add newsletter template for the contacts page

--- newsletter/forms.py
+++ newsletter/forms.py
@@ -1,7 +1,16 @@
-from django.urls import path
-from .views import SubscribeView, SubscribeSuccessView
 
-urlpatterns = [
-    path('subscribe/', SubscribeView.as_view(), name='subscribe'),
-    path('subscribe/success/', SubscribeSuccessView.as_view(), name='subscribe_success'),
-]
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit
+from django import forms
+from .models import NewsLetterMod
+
+class SubscribeForm(forms.ModelForm):
+    class Meta:
+        model = NewsLetterMod
+        fields = ['email']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.add_input(Submit('submit', 'Subscribe'))
