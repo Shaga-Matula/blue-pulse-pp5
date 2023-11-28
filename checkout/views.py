529b1715897b4a327f9455a6f868a9677b252719
Paul-Gleeson <Shagamatula@gmail.com>
2023-11-28 21:59:43 +0000

    fix(checkout): re-enable delete from bag in view

--- checkout/views.py
+++ checkout/views.py
@@ -194,6 +194,10 @@ def checkout_success(request, order_number):
         email will be sent to {order.email}.",
     )
 
+
+    if "bag" in request.session:
+        del request.session["bag"]
+
     template = "checkout/checkout_success.html"
     context = {
         "order": order,
