00ca730228049f8a1998512965a7d328b59d8f7c
Paul-Gleeson <Shagamatula@gmail.com>
2023-12-02 15:57:55 +0000

    bug(deployment):debug false

--- bluepulse/settings.py
+++ bluepulse/settings.py
@@ -21,7 +21,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# SECURITY WARNING: keep the secret used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
