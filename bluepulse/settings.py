6751957ed8b32449294bf41f0207fce9ecfaf02e
Paul-Gleeson <Shagamatula@gmail.com>
2024-01-12 03:00:45 +0000

    bug(media):Media files not in AWS

--- bluepulse/settings.py
+++ bluepulse/settings.py
@@ -194,6 +194,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+
 if 'USE_AWS' in os.environ:
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'bluepulseband'
