eb82b2209ba4ae49df258fa8ca408eaf5df5ae90
Paul-Gleeson <Shagamatula@gmail.com>
2024-07-01 20:09:52 +0000

    config update

--- bluepulse/settings.py
+++ bluepulse/settings.py
@@ -28,9 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = False
 
 ALLOWED_HOSTS = [
-    "8000-shagamatula-bluepulsepp-slb7pwv1gq0.ws-eu107.gitpod.io",
-    "8000-shagamatula-bluepulsepp-slb7pwv1gq0.ws-eu114.gitpod.io/",
-    "8000-shagamatula-bluepulsepp-w69psvyn18h.ws-eu106.gitpod.io",
+    "8000-shagamatula-bluepulsepp-slb7pwv1gq0.ws-eu114.gitpod.io",
     "bluepulsebandpp5-2efc9a1d43e2.herokuapp.com",
 ]
 
